badb312b37e658e03a27f9e5af453e9a47d1df19
Andrew Sharpe <andrew@sharpe-thinking.tech>
2022-07-13 19:13:47 +0100

    zoom and nav on screen

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector("canvas")
-const [w, h] = [window.innerWidth, window.innerHeight]
+const [w, h] = [400, 400]
 canvas.width = w
 canvas.style.width = w
 canvas.height = h
@@ -12,13 +12,32 @@ const map = (num, origMin, origMax, targMin, targMax) => {
     return targMin + targRange * (num - origMin) / origRange
 }
 
-const iterations = Math.floor(Math.random() * 100)
-const target = Math.floor(Math.random() * 1024)
-
+const iterations = 15 //Math.floor(Math.random() * 100)
+let target = 8 //Math.floor(Math.random() * 1024)
+const nav = {
+    currentZoom: 1,
+    step: 0.1,
+    ratio: 0.1,
+    position: {
+        x:0, y:0
+    },
+    updatePosition: (moveX, moveY) => {
+        console.log("nav");
+        nav.position.x += moveX / nav.currentZoom
+        nav.position.y += moveY / nav.currentZoom
+    },
+    updateZoom: zoom => {
+        console.log("zoom");
+        nav.currentZoom += nav.step
+        if (nav.currentZoom / nav.step > 20) nav.step *= 2
+        else if (nav.current / nav.step < 5) nav.step /= 2
+        target += nav.step
+    }
+}
 
 const xPixel = (x, y) => {
-    let a = map(x, 0, w, -2, 2)
-    let b = map(y, 0, h, -2, 2)
+    let a = map(x, 0, w, nav.position.x - 2/nav.currentZoom, nav.position.x + 2/nav.currentZoom)
+    let b = map(y, 0, h, nav.position.y -2/nav.currentZoom, nav.position.y + 2/nav.currentZoom)
     const [hue, bright] = buildSet(0, a, b, a, b)
     ctx.fillStyle = `hsl(${hue}, 87%, ${bright ? 50 : 0}%)`
     ctx.fillRect(x, y, 1, 1)
@@ -33,7 +52,7 @@ const yPixel = y => {
 const buildSet = (n, a, b, realNum, imagNum) => {
     realNum = realNum * realNum - imagNum * imagNum
     imagNum = 2 * realNum * imagNum
-    if(Math.abs(realNum + imagNum) > 512) {
+    if(Math.abs(realNum + imagNum) > target) {
         return [map(n, 0, iterations, 0, 255), true]
     }
     if (n < iterations) {
@@ -43,3 +62,25 @@ const buildSet = (n, a, b, realNum, imagNum) => {
 }
 
 yPixel(0)
+
+const navigate = e => {
+    const k = e.key
+    if (k === '=' || k === '+') {
+        nav.updateZoom(1) 
+    } else if(k === '-' ||k === '_') {
+        nav.updateZoom(-1) 
+    }
+    if (k === 'ArrowLeft') {
+        nav.updatePosition(-1, 0)
+    } else if (k === 'ArrowRight') {
+        nav.updatePosition(1, 0)
+    }
+    if (k === 'ArrowUp') {
+        nav.updatePosition(0, -1)
+    } else if (k === 'ArrowDown') {
+        nav.updatePosition(0, 1)
+    }
+    yPixel(0)
+}
+
+window.addEventListener('keydown', navigate)
